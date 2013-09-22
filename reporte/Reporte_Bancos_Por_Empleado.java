380630b1b9c0166ff627f72a50163c2a15a7e38f
mabg1 <mabg161194mmn@hotmail.com>
2013-09-22 15:24:30 -0600

    reporte de asistencia

--- reporte/Reporte_Bancos_Por_Empleado.java
+++ reporte/Reporte_Bancos_Por_Empleado.java
@@ -1,7 +1,6 @@
 package reporte;
 
-import java.sql.ResultSet;
-import java.sql.Statement;
+
 import java.util.HashMap;
 
 import javax.swing.JFrame;
@@ -10,7 +9,7 @@ import SQL.Connexion;
 
 
 
-import net.sf.jasperreports.engine.JRResultSetDataSource;
+
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -20,42 +19,15 @@ import net.sf.jasperreports.view.JasperViewer;
 @SuppressWarnings("serial")
 public class Reporte_Bancos_Por_Empleado extends JFrame {
 
-//	@SuppressWarnings({ "rawtypes", "unchecked" })
-//	public Reporte_Bancos_Por_Empleado() {
-//		try {
-//			JasperReport report = JasperCompileManager.compileReport(System.getProperty("user.dir")+"\\src\\Reportes\\Reporte_Bancos_Por_Empleado.jrxml");
-//			JasperPrint print = JasperFillManager.fillReport(report, new HashMap(), new Connexion().conexion());
-//			JasperViewer.viewReport(print, false);
-//		} catch (Exception e) {
-//			System.out.println(e.getMessage());
-//		}
-//	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public Reporte_Bancos_Por_Empleado(int folio, String NOMBRE){
-		String query = "select tb_empleado.folio as folio, "+
-	   "tb_empleado.nombre + ' ' + tb_empleado.ap_paterno + ' ' + tb_empleado.ap_materno as NombreCompleto, "+
-	   "tb_establecimiento.nombre as Establecimiento "+
-"from tb_empleado "+
-"left join tb_establecimiento on tb_establecimiento.folio = tb_empleado.establecimiento_id  "+
-
-"where tb_empleado.folio < "+folio +"and tb_empleado.nombre='"+ NOMBRE+"'";
-		Statement stmt = null;
-		try{
-			stmt =  new Connexion().conexion().createStatement();
-		    ResultSet rs = stmt.executeQuery(query);
-		    
-			JasperReport report = JasperCompileManager.compileReport(System.getProperty("user.dir")+"\\Reportes\\Silhouette.jrxml");
-			JRResultSetDataSource resultSetDataSource = new JRResultSetDataSource(rs);
-			JasperPrint print = JasperFillManager.fillReport(report, new HashMap(),resultSetDataSource);
-			JasperViewer.viewReport(print,false);
-		}catch(Exception e){
-			System.err.print("Error de reporte Bancos por Empleado"+e.getMessage());
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Reporte_Bancos_Por_Empleado() {
+		try {
+			JasperReport report = JasperCompileManager.compileReport(System.getProperty("user.dir")+"\\src\\Reportes\\Reporte_Bancos_Por_Empleado.jrxml");
+			JasperPrint print = JasperFillManager.fillReport(report, new HashMap(), new Connexion().conexion());
+			JasperViewer.viewReport(print, false);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 	
-	public static void main(String args[]){
-		new Reporte_Bancos_Por_Empleado(500,"EDGAR");
-	}
-
 }
--- reporte/Reporte_General_de_Asistencia_Por_Establecimiento.java
+++ reporte/Reporte_General_de_Asistencia_Por_Establecimiento.java
@@ -1,14 +1,16 @@
 package reporte;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.HashMap;
 
 import javax.swing.JFrame;
-import javax.swing.UIManager;
 
 import SQL.Connexion;
 
 
 
+import net.sf.jasperreports.engine.JRResultSetDataSource;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -17,24 +19,26 @@ import net.sf.jasperreports.view.JasperViewer;
 
 @SuppressWarnings("serial")
 public class Reporte_General_de_Asistencia_Por_Establecimiento extends JFrame {
+	
+	
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public Reporte_General_de_Asistencia_Por_Establecimiento() {
+	public Reporte_General_de_Asistencia_Por_Establecimiento(String fecha_inicio, String fecha_final) {
+		String query = "exec sp_Reporte_General_de_Asistencia_Por_Establecimiento '"+fecha_inicio+"','"+fecha_final+"';";
+		Statement stmt = null;
+		System.out.println(query);
 		try {
+			stmt =  new Connexion().conexion().createStatement();
+		    ResultSet rs = stmt.executeQuery(query);
+		    
 			JasperReport report = JasperCompileManager.compileReport(System.getProperty("user.dir")+"\\src\\Reportes\\Reporte_General_de_Asistencia_Por_Establecimiento.jrxml");
-			JasperPrint print = JasperFillManager.fillReport(report, new HashMap(), new Connexion().conexion());
+			JRResultSetDataSource resultSetDataSource = new JRResultSetDataSource(rs);
+			JasperPrint print = JasperFillManager.fillReport(report, new HashMap(), resultSetDataSource);
 			JasperViewer.viewReport(print, false);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	public static void main(String args[]){
-		try{
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			new Reporte_General_de_Asistencia_Por_Establecimiento();
-		}catch(Exception e){
-			
-		}
-		
-	}
+
 }
+
